6c1ec9866f939158f69a02e8c11398a8cd1bcf23
gstvomacedo <gstvomacedo@gmail.com>
2021-08-29 23:02:47 -0300

    Adicionando retorno da compra criada após criação

--- app/orders/controller/orders.controller.ts
+++ app/orders/controller/orders.controller.ts
@@ -9,10 +9,10 @@ const log: debug.IDebugger = debug("app:orders-controller");
 class OrdersController {
   async addOrder(req: Request, res: Response) {
     try {
-      const order = await ordersService.create(req.body);
+      const createdOrder = await ordersService.create(req.body);
 
-      if (order) {
-        res.status(httpStatusCode.CREATED).send();
+      if (createdOrder) {
+        res.status(httpStatusCode.CREATED).send(createdOrder);
       } else {
         res.status(httpStatusCode.BAD_REQUEST).send({
           error: "Não foi possível registrar a compra"
